c7a6cef46e3ed1895c53cb9b2edbcdd451b8b9d4
Jesse Michael <jesse.michael@faithlife.com>
2019-07-17 22:50:17 -0700

    Upgrade to unity 2019 1.4

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -2,162 +2,182 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Hero : MonoBehaviour {
+public class Hero : MonoBehaviour
+{
 
-	public static float health = 40.0f;
-	bool fired = false;
-	bool firing = false;
-	float reloadTime = 1.5f;
+    public static float health = 40.0f;
+    bool fired = false;
+    bool firing = false;
+    float reloadTime = 1.5f;
 
-	Vector2 startPos;
-	Vector2 endPos;
-
-
-	Animator bowAnim;
-	Transform bowTran;
-	Transform arrowTran;
-	Transform arrowSpawn;
-	HeroArm hand;
-
-	float distance = 0.0f;
-	float angle = 0.0f;
-	Vector2 direction;
-
-	Rigidbody2D arrowInstance;
-	public Rigidbody2D arrowType;
-
-	float playTime = 0.0f;
-
-	static Color damageColor;
-
-	static GUITexture damageTexture;
-	public GUITexture textureType;
-
-	public GameObject aimingPress;
-	public GameObject aimingRelease;
+    Vector2 startPos;
+    Vector2 endPos;
 
 
-	// Use this for initialization
-	void Start () {
+    Animator bowAnim;
+    Transform bowTran;
+    Transform arrowTran;
+    Transform arrowSpawn;
+    HeroArm hand;
 
-		arrowTran = transform.Find("heroarrow");
-		bowTran = transform.Find("HeroArm");
-		arrowSpawn = bowTran.Find("heroarm").Find("arrowSpawn");
-		bowAnim = bowTran.GetComponent<Animator>();
-		hand = bowTran.GetComponent ("HeroArm") as HeroArm;
-		hand.HideHand();
+    float distance = 0.0f;
+    float angle = 0.0f;
+    Vector2 direction;
 
-		damageTexture = GameObject.Instantiate (textureType, new Vector3 (0, 0, 0), Quaternion.Euler (new Vector3 (0, 0, 0))) as GUITexture;
-		damageColor = new Color (1,1,1,0);
-		damageTexture.color = damageColor;
-	}
-	void OnLevelWasLoaded(int level) {
-		health = 40.0f;
-		playTime = 0.0f;
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    Rigidbody2D arrowInstance;
+    public Rigidbody2D arrowType;
 
-		playTime += Time.deltaTime;
+    float playTime = 0.0f;
 
-		if (fired) {
-			reloadTime -= Time.deltaTime;
+    static Color damageColor;
 
-			if (reloadTime <= 0.0f) {
-				fired = false;
-				reloadTime = 1.5f;
-				arrowTran.GetComponent<Renderer>().enabled = true;
-			}
-		}   
+    static GUITexture damageTexture;
+    public GUITexture textureType;
 
-		if (Input.GetMouseButtonUp (0)) {
-			OnMouseUpF();
-		}
-		else if (Input.GetMouseButton (0)) {
-			OnMouseDownF();
-		}
+    public GameObject aimingPress;
+    public GameObject aimingRelease;
 
 
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-	void OnMouseDownF() {
-		if (!fired && !firing) {
-				arrowInstance = Instantiate (arrowType, arrowSpawn.position, Quaternion.Euler (new Vector3 (0, 0, 0))) as Rigidbody2D;
-				arrowTran.GetComponent<Renderer>().enabled = false;
-				bowAnim.SetTrigger ("pull");
-				firing = true;
-				startPos = Input.mousePosition;
-				hand.ShowHand ();
-		} else {
-			OnMouseDragF();
-		}
-	}
+        arrowTran = transform.Find("heroarrow");
+        bowTran = transform.Find("HeroArm");
+        arrowSpawn = bowTran.Find("heroarm").Find("arrowSpawn");
+        bowAnim = bowTran.GetComponent<Animator>();
+        hand = bowTran.GetComponent("HeroArm") as HeroArm;
+        hand.HideHand();
 
-	void OnMouseUpF () {
-		if (firing) {
-			bowAnim.SetTrigger("release");
-			firing = false;
-			fired = true;
+        damageTexture = GameObject.Instantiate(textureType, new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 0, 0))) as GUITexture;
+        damageColor = new Color(1, 1, 1, 0);
+        damageTexture.color = damageColor;
+        health = 40.0f;
+        playTime = 0.0f;
+    }
+    // void OnLevelWasLoaded(int level) {
+    // 	health = 40.0f;
+    // 	playTime = 0.0f;
 
-			GameObject[] myArrows = GameObject.FindGameObjectsWithTag("Arrow");
+    // }
 
-			foreach(GameObject arrow in myArrows)
-			{
-				Arrow ar = arrow.GetComponent("Arrow")as Arrow;
+    // Update is called once per frame
+    void Update()
+    {
 
-				if(ar != null && !ar.isFlying)
-				{
-					distance = Mathf.Max(300.0f, distance);
-					distance = Mathf.Min(800.0f, distance);
-					ar.Launch(direction, distance);
+        playTime += Time.deltaTime;
 
-				}
-			}
+        if (fired)
+        {
+            reloadTime -= Time.deltaTime;
 
-		}
+            if (reloadTime <= 0.0f)
+            {
+                fired = false;
+                reloadTime = 1.5f;
+                arrowTran.GetComponent<Renderer>().enabled = true;
+            }
+        }
 
-	}
-
-	void OnMouseDragF () {
-		if (firing) {
-			endPos = Input.mousePosition;
-
-			direction = (startPos - endPos);
-			
-			angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-			distance = Vector2.Distance(endPos, startPos);
-
-			if(arrowInstance != null)
-			{
-				arrowInstance.transform.position = arrowSpawn.position;
-				arrowInstance.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-			}
-
-			bowTran.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-		}
-
-	}
+        if (Input.GetMouseButtonUp(0))
+        {
+            OnMouseUpF();
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            OnMouseDownF();
+        }
 
 
+    }
 
-	public static void GetHit(float damage){
-		health -= damage;
+    void OnMouseDownF()
+    {
+        if (!fired && !firing)
+        {
+            arrowInstance = Instantiate(arrowType, arrowSpawn.position, Quaternion.Euler(new Vector3(0, 0, 0))) as Rigidbody2D;
+            arrowTran.GetComponent<Renderer>().enabled = false;
+            bowAnim.SetTrigger("pull");
+            firing = true;
+            startPos = Input.mousePosition;
+            hand.ShowHand();
+        }
+        else
+        {
+            OnMouseDragF();
+        }
+    }
 
-		damageColor = new Color (1,1,1,(40.0f - health) / 100f);
-		damageTexture.color = damageColor;
+    void OnMouseUpF()
+    {
+        if (firing)
+        {
+            bowAnim.SetTrigger("release");
+            firing = false;
+            fired = true;
 
-		#if(UNITY_ANDROID || UNITY_IOS || UNITY_WP8) 
+            GameObject[] myArrows = GameObject.FindGameObjectsWithTag("Arrow");
+
+            foreach (GameObject arrow in myArrows)
+            {
+                Arrow ar = arrow.GetComponent("Arrow") as Arrow;
+
+                if (ar != null && !ar.isFlying)
+                {
+                    distance = Mathf.Max(300.0f, distance);
+                    distance = Mathf.Min(800.0f, distance);
+                    ar.Launch(direction, distance);
+
+                }
+            }
+
+        }
+
+    }
+
+    void OnMouseDragF()
+    {
+        if (firing)
+        {
+            endPos = Input.mousePosition;
+
+            direction = (startPos - endPos);
+
+            angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            distance = Vector2.Distance(endPos, startPos);
+
+            if (arrowInstance != null)
+            {
+                arrowInstance.transform.position = arrowSpawn.position;
+                arrowInstance.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }
+
+            bowTran.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+
+    }
+
+
+
+    public static void GetHit(float damage)
+    {
+        health -= damage;
+
+        damageColor = new Color(1, 1, 1, (40.0f - health) / 100f);
+        damageTexture.color = damageColor;
+
+#if (UNITY_ANDROID || UNITY_IOS || UNITY_WP8)
 			Handheld.Vibrate();
-		#endif
+#endif
 
-		if (health <= 0.0f) {
-			GameOver();
-		}
-	}
+        if (health <= 0.0f)
+        {
+            GameOver();
+        }
+    }
 
-	static void GameOver(){
-		Application.LoadLevel("GameOver");
-	}
+    static void GameOver()
+    {
+        Application.LoadLevel("GameOver");
+    }
 }
---
+++ README.md
@@ -0,0 +1,9 @@
+## Beneath The Surface
+
+The theme for Ludum Dare 29 was "Beneath the Surface".
+
+Fend off what lies beneath the surface.
+
+You play a hero that wields a bow and fends of a giant for as long as you can. To fire an arrow, click on the scene and drag the mouse as if you were pulling back the arrow on the bow. The game use the angle from where you started and ended clicking, as well as the distance of the two points, to calculate a trajectory for the arrow. You must hit the giant to stop him from destroying you.
+
+ranger, monster, ludumdare, ld
